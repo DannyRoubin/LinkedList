2b6b2d7b749b5582466f086a209731e627608aa6
DannyRoubin <Dannyroubin1@gmail.com>
2021-11-17 01:17:39 -0800

    transferring files

--- List.java
+++ List.java
@@ -6,6 +6,9 @@ Assignment: Linked List assignment
 Purpose of this file/class is to act as a linked list superclass that will be extended to
 a stack and queue subclasses
 */
+
+//************Before finishing make sure I go through and test every single possibility for insert and remove */
+
 public class List {
 
     // setting local var head null by default
@@ -30,11 +33,10 @@ public class List {
             System.out.println("Throw exception temp holder");
         }
 
-        // ****Come back to this once I have size built out****
         // Makes sure that the size is smaller than the index
-        // else if( size() < index ) {
-        // System.out.println("Throw exception temp holder");
-        // }
+        else if (size() < index) {
+            System.out.println("Throw exception temp holder");
+        }
 
         // ****Section that checks for an empty list****
         // first we check if the list is null and if the index is over 0
@@ -46,7 +48,7 @@ public class List {
             head = newNode;
         }
 
-        // ****Section that checks a list of length a single length****
+        // ****Section that checks a list that only has a single Node****
         // first checking if the list only has one element and if the index is greater
         // than 1, since that wouldn't be allowed
         else if (head.next == null && index > 1) {
@@ -87,11 +89,97 @@ public class List {
 
     }
 
+    public Object remove(int index) {
+        // We set up all of the else if's so we only hit one statement
+        // no possible way for the index to be less than 0, so check for this first and
+        // throw error if it is the case
+        if (index < 0) {
+            System.out.println("Throw exception temp holder");
+        }
+
+        // Makes sure that the size is smaller than the index
+        else if (size() < index) {
+            System.out.println("Throw exception temp holder");
+        }
+
+        // ****Section that checks for an empty list****
+        // first we check if the list is empty and throw exception since that wont work
+        // with us
+        else if (head == null) {
+            System.out.println("Throws exception temp holder");
+        }
+        // ****Section that checks a list that only has a single Node****
+        // first checking if the list only has one element and if the index is greater
+        // than 1, since that wouldn't be allowed
+        else if (head.next == null && index > 0) {
+            System.out.println("Throw exception temp holder");
+        }
+        // valid case of if there is one element and the desired index is 0, we replace
+        // the head with next and have it point to the old head
+        else if (head.next == null && index == 0) {
+            Object temp = head.data;
+            head = null;
+            return temp;
+        }
+        // ****Section that checks a list of any length greater than 1 ****
+        // Checks to see if we are updating the head
+        else if (index == 0) {
+            Object temp = head.data;
+            Node newHead = head.next;
+            head = newHead;
+            return temp;
+        }
+
+        // Next we can check if we are deleting the last index
+        else if (index == size()) {
+            Node prev = head;
+            Node nextIndexHolder = head.next;
+            while (nextIndexHolder.next != null) {
+                prev = prev.next;
+                nextIndexHolder = nextIndexHolder.next;
+            }
+            Object temp = nextIndexHolder.data;
+            prev.next = null;
+            return temp;
+        }
+        // finally we can remove a specified index
+        else {
+            Node prev = head;
+            Node nextIndexHolder = head.next;
+            for (int i = 1; i < index; i++) {
+                prev = prev.next;
+                nextIndexHolder = nextIndexHolder.next;
+            }
+            Object temp = nextIndexHolder.data;
+            prev.next = nextIndexHolder.next;
+            return temp;
+        }
+        return null;
+
+    }
+
+    public int size() {
+        if (head == null) {
+            return 0;
+        }
+        int count = 0;
+
+        Node current = head;
+
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+
+    }
+
     public String toString() {
         Node current = head;
         String retValue = "";
 
-        while(current != null) {
+        while (current != null) {
             retValue += current.data.toString() + " ";
             current = current.next;
         }
@@ -105,24 +193,46 @@ public class List {
         List multiple = new List();
         // try {
         one.insert(5, 0);
+        // trying out inserting at start
         multiple.insert(10, 0);
         multiple.insert(20, 0);
         multiple.insert(30, 0);
+        // trying out inserting in the middle
         multiple.insert(7, 1);
         multiple.insert(8, 3);
+        // testing edge case with adding
+        multiple.insert(9, 5);
+        // testing out of bounds by 1 case
+        multiple.insert(3, 7);
+
         System.out.println("Empty:" + empty);
         System.out.println("One:" + one);
         System.out.println("Multiple:" + multiple);
-        // one.remove(0);
-        // multiple.remove(1);
-        // System.out.println("One (upon remove 1):" + one);
-        // System.out.println("Multiple (upon remove):" + multiple);
-        // one.insert(600, 1);
-        // multiple.insert(400, 2);
-        // System.out.println("One (on insert at 1):" + one);
-        // System.out.println("Multiple(on insert at 2):" + multiple);
-        // // test error
-        // // empty.remove(3);
+
+        // removal of first and only node
+        one.remove(0);
+        // removing first case again
+        multiple.remove(0);
+        // removing in the center
+        multiple.remove(1);
+        // removing end case
+        multiple.remove(4);
+
+        System.out.println("One (upon remove 1):" + one);
+        System.out.println("Multiple (upon remove):" + multiple);
+
+        multiple.insert(400, 2);
+        System.out.println("One (on insert at 1):" + one);
+        System.out.println("Multiple(on insert at 2):" + multiple);
+        // test error
+        empty.remove(3);
+        // tripping error on inputting out of bounds
+        System.out.println("Next line will be an error");
+        one.insert(600, 1);
+        // tripping an error by inserting at a negative position
+        one.insert(26, -7);
+        // tripping an error by removing a negative position
+        empty.remove(-9);
         // } catch (LinkedListException e) {
         // System.out.println(e.getMessage());
         // }
