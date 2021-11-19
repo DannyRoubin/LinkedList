2697ab3ae87fa9735023a5a00a09b6da3348e452
DannyRoubin <Dannyroubin1@gmail.com>
2021-11-18 16:33:08 -0800

    about 99% done with list class just need to add exceptions

--- List.java
+++ List.java
@@ -158,15 +158,19 @@ public class List {
 
     }
 
+    // method that finds out the size of the linked list
     public int size() {
         if (head == null) {
             return 0;
         }
+        // starts counter at 0 to act as an array
         int count = 0;
 
         Node current = head;
 
+        // increment through the list while current isn't null
         while (current != null) {
+            // each time increment the count and assign current to the next node
             count++;
             current = current.next;
         }
@@ -175,6 +179,7 @@ public class List {
 
     }
 
+    // returns the linked list in the form of a string for readability 
     public String toString() {
         Node current = head;
         String retValue = "";
@@ -187,7 +192,9 @@ public class List {
 
     }
 
+    // checks if the linked list is empty or not
     public boolean isEmpty() {
+        // returns true if it is empty, false otherwise
         if (size() == 0) {
             return true;
         } else {
@@ -195,6 +202,33 @@ public class List {
         }
     }
 
+    // searches the linked list and attempts to find the index of the given target if it exists
+    public int indexOf(Object target) {
+        // if list is empty auto return -1
+        if (isEmpty()) {
+            return -1;
+        } else {
+            // otherwise loop through until the target matches the data of the node at the current index
+            Node current = head;
+            int indexTracker = 0;
+            while (current != null) {
+                if (target.equals(current.data)) {
+                    return indexTracker;
+                }
+                current = current.next;
+                indexTracker++;
+            }
+            // return -1 if the whole list has been searched without a find
+            return -1;
+        }
+    }
+
+    // adds to the end of the linked list by reusing the size and insert methods we created
+    public void addToEnd(Object obj) {
+        insert(obj,size());
+    }
+
+    // main method used for testing every possible output
     public static void main(String[] args) {
         List empty = new List();
         List one = new List();
@@ -205,32 +239,33 @@ public class List {
         System.out.println("*********Testing all cases with inserting*********");
         System.out.println("");
         one.insert(5, 0);
-        System.out.println("list One after a single insert at index 0, expecting 5 :" + one);
+        System.out.println("list One after a single insert at index 0, expecting 5 : " + one);
 
         // trying out inserting multiple nodes to the front
-        System.out.println("Multiple list prior to any changes, expecting empty :" + multiple);
+        System.out.println("Multiple list prior to any changes, expecting empty : " + multiple);
         multiple.insert(10, 0);
         multiple.insert(20, 0);
         multiple.insert(30, 0);
-        System.out.println("Multiple list post inserting multiple nodes in the beginning, expecting 30 20 10 :" + multiple);
+        System.out.println("Multiple list post inserting multiple nodes in the beginning, expecting 30 20 10 : " + multiple);
         // trying out inserting in the middle
         multiple.insert(7, 1);
         multiple.insert(8, 3);
-        System.out.println("Multiple list post inserting multiple nodes at index 1 and index 3 in the list, expecting 30 7 20 8 10 :" + multiple);
+        System.out.println("Multiple list post inserting multiple nodes at index 1 and index 3 in the list, expecting 30 7 20 8 10 : " + multiple);
         // testing edge case with adding
         multiple.insert(9, 5);
-        System.out.println("Multiple list post testing edge case with adding 9 as the new tail for the linked list, expecting 30 7 20 8 10 9 :" + multiple);
+        System.out.println("Multiple list post testing edge case with adding 9 as the new tail for the linked list, expecting 30 7 20 8 10 9 : " + multiple);
         // testing out of bounds by 1 case
-        System.out.println("Multiple list testing adding a new node 1 index too far out of bounds, expecting error on next line :" );
+        System.out.println("Multiple list testing adding a new node 1 index too far out of bounds, expecting error on next line : " );
         multiple.insert(3, 7);
-        // testing inserting at ta negative index
-        System.out.println("Multiple list testing adding a new node at a negative index, expecting error on next line :" );
+        // testing inserting at a negative index
+        System.out.println("Multiple list testing adding a new node at a negative index, expecting error on next line : " );
         multiple.insert(3, -1);
 
         System.out.println("Relisting the three lists we have to document them prior to any removal testing is done" );
-        System.out.println("List Empty, expecting empty :" + empty);
-        System.out.println("List One, expecting 5 :" + one);
-        System.out.println("List Multiple, expecting 30 7 20 8 10 9 :" + multiple);
+        System.out.println("List Empty, expecting empty : " + empty);
+        System.out.println("List One, expecting 5 : " + one);
+        System.out.println("List Multiple, expecting 30 7 20 8 10 9 : " + multiple);
+        
 
         System.out.println("");
         System.out.println("*********Testing all cases with removing*********");
@@ -238,39 +273,78 @@ public class List {
 
         // testing fail cases first this time around
         // first testing if we are able to remove at a negative index
-        System.out.println("Multiple list testing removing a node at a negative index, expecting error on next line :" );
+        System.out.println("Multiple list testing removing a node at a negative index, expecting error on next line : " );
         multiple.remove(-1);
         // testing if we are able to remove at an out of bounds index
-        System.out.println("Multiple list testing removing a node at an out of bounds index, expecting error on next line :" );
+        System.out.println("Multiple list testing removing a node at an out of bounds index, expecting error on next line : " );
         multiple.remove(15);
+        // removal of node from an empty list
+        System.out.println("Empty list testing removing a node from an empty, expecting an error on the next line : " );
+        empty.remove(0);
 
         // removal of first and only node
         one.remove(0);
+        System.out.println("One list testing removing a node from a list with a single index, expecting an empty list : " + one );
         // removing first case again
-        multiple.remove(0);
+        multiple.remove(5);
+        System.out.println("multiple list testing removing the last node(the tail) from a list, expecting 30 7 20 8 10: " + multiple );
         // removing in the center
         multiple.remove(1);
-        // removing end case
-        multiple.remove(4);
+        System.out.println("multiple list testing removing a node from the middle of a list, expecting 30 20 8 10: " + multiple );
+      
+        System.out.println("");
+        System.out.println("*********Testing all cases for the method indexOf*********");
+        System.out.println("");
+     
+        System.out.println("Relisting the list multiple so we can document it prior to any testing is done" );
+        System.out.println("List Multiple, expecting 30 20 8 10  : " + multiple);
+        System.out.println("");
 
-        System.out.println("One (upon remove 1):" + one);
-        System.out.println("Multiple (upon remove):" + multiple);
+        // working on finding a point in the middle of the list
+        System.out.println("Multiple list testing finding something in the middle of the list by finding the indexOf 8, expecting 2 : " + multiple.indexOf(8));
+        // working on finding a point at the beginning of a list
+        System.out.println("Multiple list testing finding something at the beginning of the list by finding the indexOf 30, expecting 0 : " + multiple.indexOf(30));
+        // working on finding a point at the end of a list
+        System.out.println("Multiple list testing finding something at the end of the list by finding the indexOf 10, expecting 3 : " + multiple.indexOf(10));
+        // working on finding a point that is non existent in the list
+        System.out.println("Multiple list testing finding something that doesn't exist in the list by finding the indexOf 5400, expecting -1 : " + multiple.indexOf(5400));
+     
+        System.out.println("");
+        System.out.println("*********Testing all cases for the method addToEnd*********");
+        System.out.println("");
 
-        multiple.insert(400, 2);
-        System.out.println("One (on insert at 1):" + one);
-        System.out.println("Multiple(on insert at 2):" + multiple);
-        // test error
-        empty.remove(3);
-        // tripping error on inputting out of bounds
-        System.out.println("Next line will be an error");
-        one.insert(600, 1);
-        // tripping an error by inserting at a negative position
-        one.insert(26, -7);
-        // tripping an error by removing a negative position
-        empty.remove(-9);
-        // } catch (LinkedListException e) {
-        // System.out.println(e.getMessage());
-        // }
+        System.out.println("Relisting the list multiple so we can document it prior to any testing is done" );
+        System.out.println("List Multiple, expecting 30 20 8 10  : " + multiple);
+        System.out.println("List One, expecting empty : " + one);
+        System.out.println("");
+
+        // adding 22 to the end of the multiple list
+        multiple.addToEnd(22);
+        System.out.println("List Multiple after testing addToEnd, expecting 30 20 8 10 22  : " + multiple);
+        // adding to the end of an empty list
+        one.addToEnd(66);
+        System.out.println("List one after testing addToEnd on an empty list, expecting 66  : " + one);
+        // adding to the end of a list with a single index
+        one.addToEnd(11);
+        System.out.println("List one after testing addToEnd on a list with a single node, expecting 66 11  : " + one);
+
+        System.out.println("");
+        System.out.println("*********Testing all cases for the methods size and isEmpty*********");
+        System.out.println("");
+
+        System.out.println("Relisting the list multiple so we can document it prior to any testing is done" );
+        System.out.println("List Empty, expecting empty : " + empty);
+        System.out.println("List Multiple, expecting 30 20 8 10 22 : " + multiple);
+        System.out.println("List One, expecting 66 11 : " + one);
+        System.out.println("");
+
+        
+        System.out.println("Testing size method on list one which has a size of 2 expecting 2  : " + one.size());
+        System.out.println("Testing size method on list multiple which has a size of 5 expecting 5  : " + multiple.size());
+        System.out.println("Testing size method on the empty list expecting 0  : " + empty.size());
+        System.out.println("Testing isEmpty method on the empty list expecting true  : " + empty.isEmpty());
+        System.out.println("Testing isEmpty method on the list one which has2 nodes in it, expecting false  : " + one.isEmpty());
+
     }
 
 }
